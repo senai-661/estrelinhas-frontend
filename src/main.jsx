a2baa571e8e2fbb064a14240930330ddd2f87541
TAUANE VITÓRIA SILVA SOUZA <tauane.souza@portalsesisp.org.br>
2026-04-24 14:00:13 -0300

    feat: ajustando listagem
    ajustando listagem de alunos

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'primereact/resources/themes/lara-light-indigo/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
