c9a1066f8a9b4f3afce3bc2f8edc49fd8a0c9f5f
LAÍS ZANQUETA <lais.zanqueta@portalsesisp.org.br>
2026-04-24 09:31:34 -0300

    feat: s8 - criando arquivos
    criando arquivos DTO, aluno, matricula e plano

---
+++ interface-gympro/src/components/dto/PlanoDTO.tsx
@@ -0,0 +1,10 @@
+export interface PlanoDTO {
+
+    cod_plano: string;      
+    tipo_plano: string;     
+    duracao_dias: number;  
+    valor: number;         
+    descricao?: string;     
+    status_plano?: string; 
+    
+}
