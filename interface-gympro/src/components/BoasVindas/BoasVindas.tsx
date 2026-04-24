64ac98afeab70f4a215fc1812979a61a73119ce6
SOPHIA ISABELLE RODRIGUES GUMBIO <sophia.gumbio@portalsesisp.org.br>
2026-04-24 13:38:18 -0300

    feat: cria tela de boas-vindas
    Adiciona componente BoasVindas com layout inicial da página

---
+++ interface-gympro/src/components/BoasVindas/BoasVindas.tsx
@@ -0,0 +1,18 @@
+import { type JSX } from "react";
+
+function BoasVindas(): JSX.Element {
+    return (
+        <main className="bg-gray-200  h-[76vh]">
+            <h1 className="text-[3rem] pt-20" style={{ textAlign: 'center' }}>GymPro</h1>
+
+            <p className="text-[1.2rem] mt-10" style={{ textAlign: 'center' }}>
+                A GymPro é uma academia focada no seu desempenho e bem-estar. 
+                Aqui você encontra treinos personalizados, acompanhamento profissional e uma estrutura pensada para te ajudar 
+                a alcançar seus objetivos de forma prática e eficiente.
+                 Seja para ganhar massa, emagrecer ou melhorar sua saúde, a GymPro está com você em cada etapa.
+            </p>
+        </main>
+    );
+}
+
+export default BoasVindas;
