cd6f166204945b32264c255908489f1385cb0cb5
TAUANE VITÓRIA SILVA SOUZA <tauane.souza@portalsesisp.org.br>
2026-04-29 08:39:35 -0300

    feat: adiciona MatriculaDTO
    cria interface para matrícula

--- src/dto/MatriculaDTO.ts
+++ src/dto/MatriculaDTO.ts
@@ -1,28 +1,30 @@
 export default interface MatriculaDTO {
-    id_matricula: number,
-
+    id_matricula?: number,
+    cod_matricula?: string,
     aluno: {
         id_aluno: number,
+        cod_aluno?: string,
         nome?: string,
         sobrenome?: string,
+        cpf?: string,
         data_nascimento?: Date,
         endereco?: string,
         email?: string,
         celular?: string,
-        status_aluno?: boolean
+        status_aluno?: string
     },
-
     plano: {
         id_plano: number,
-        nome?: string,
-        descricao?: string,
+        cod_plano?: string,
+        tipo_plano?: string,
         duracao_dias?: number,
         valor?: number,
-        ativo?: boolean
+        descricao?: string,
+        status_plano?: string
     },
-
     data_inicio: Date,
-    data_fim?: Date,
+    data_fim: Date,
     status_matricula?: string,
-    ativo?: boolean
+    forma_pagamento?: string,
+    valor_final?: number
 }
