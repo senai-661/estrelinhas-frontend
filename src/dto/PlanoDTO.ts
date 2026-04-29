5cf2e23810b7165442f2e8c9b8c4b5d608cbda9b
TAUANE VITÓRIA SILVA SOUZA <tauane.souza@portalsesisp.org.br>
2026-04-29 08:40:15 -0300

    feat: adiciona PlanoDTO
    cria interface para plano

--- src/dto/PlanoDTO.ts
+++ src/dto/PlanoDTO.ts
@@ -1,13 +1,9 @@
 export default interface PlanoDTO {
-    id_plano: number,
-    nome: string,
-    descricao?: string,
+    id_plano?: number,
+    cod_plano?: string,
+    tipo_plano: string,
     duracao_dias: number,
     valor: number,
-    acesso_total?: boolean, // acesso a todas as áreas
-    horario_inicio?: string, // ex: "06:00"
-    horario_fim?: string,    // ex: "22:00"
-    limite_alunos?: number,
-    status_plano?: string, // ex: "ativo", "inativo"
-    ativo?: boolean
+    descricao?: string,
+    status_plano?: string
 }
