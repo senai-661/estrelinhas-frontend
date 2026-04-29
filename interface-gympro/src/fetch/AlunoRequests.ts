c1f3ba065a93ff50976371deb3d844aadfe0db6b
SOPHIA ISABELLE RODRIGUES GUMBIO <sophia.gumbio@portalsesisp.org.br>
2026-04-29 08:17:51 -0300

    feat: criando pagina do css
    Adicionado FormLogin css

---
+++ interface-gympro/src/fetch/AlunoRequests.ts
@@ -0,0 +1,61 @@
+import type AlunoDTO from "../components/dto/AlunoDTO";
+class AlunoRequests {
+    private serverURL: string;
+    private endpointAluno: string;
+
+    constructor() {
+        this.serverURL = 'http://localhost:3333';
+        this.endpointAluno = '/api/alunos';
+    }
+
+
+    async obterListaDeAlunos(): Promise<AlunoDTO[] | undefined> {
+        try {
+            const token = localStorage.getItem('token');
+
+            const respostaAPI = await fetch(`${this.serverURL}${this.endpointAluno}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-access-token': `${token}`
+                }
+            });
+
+            if (respostaAPI.ok) {
+                const lista: AlunoDTO[] = await respostaAPI.json();
+                return lista;
+            } else {
+                throw new Error("Não foi possível listar os alunos.");
+            }
+        } catch (error) {
+            console.error(`Erro ao buscar alunos: ${error}`);
+        }
+    }
+
+
+    async cadastrarAluno(aluno: AlunoDTO): Promise<AlunoDTO | undefined> {
+        try {
+            const token = localStorage.getItem('token');
+
+            const respostaAPI = await fetch(`${this.serverURL}${this.endpointAluno}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-access-token': `${token}`
+                },
+                body: JSON.stringify(aluno)
+            });
+
+            if (respostaAPI.ok) {
+                const novoAluno: AlunoDTO = await respostaAPI.json();
+                return novoAluno;
+            } else {
+                throw new Error("Erro ao cadastrar aluno.");
+            }
+        } catch (error) {
+            console.error(`Erro ao cadastrar aluno: ${error}`);
+        }
+    }
+}
+
+export default AlunoRequests;
