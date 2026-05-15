0045537d51162bcbb325111688ca3869755627a9
TAUANE VITÓRIA SILVA SOUZA <tauane.souza@portalsesisp.org.br>
2026-05-15 08:33:27 -0300

    feat: listagem de alunos

--- src/components/FormLogin/FormLogin.tsx
+++ src/components/FormLogin/FormLogin.tsx
@@ -1,6 +1,6 @@
 import { type JSX, useState } from 'react';
 import estilo from './FormLogin.module.css';
-import AuthRequests from '../../../fetch/AuthRequests';
+import AuthRequests from '../../services/AuthRequests';
 
 function FormLogin(): JSX.Element {
     const [email, setEmail] = useState('');
