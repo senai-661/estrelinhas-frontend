99a483e106598ea15b739b1675fa0968f06a2f15
LAÍS ZANQUETA <lais.zanqueta@portalsesisp.org.br>
2026-05-29 10:49:55 -0300

    arrumando rodape do login

--- interface-gympro/src/pages/PLogin/PLogin.tsx
+++ interface-gympro/src/pages/PLogin/PLogin.tsx
@@ -1,10 +1,12 @@
 import { type JSX } from "react";
 import LoginForm from "../../components/FormLogin/FormLogin";
+import Rodape from "../../components/Rodape/Rodape";
 
 function PLogin(): JSX.Element {
     return (
         <main className="pagina-grid">
             <LoginForm />
+            <Rodape />
         </main>
     );
 }
